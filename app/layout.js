42108673165295b04336af2e4494bc6ab60039e4
syedmaazsaeed <syedmaazsaeed@gmail.com>
2025-02-12 13:11:15 +0500

    First Commit

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,14 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+      <pre>Software Engineer</pre>
     </html>
   );
 }
+
+
+//  Layout.js is the main entry point of the next js app.
+//  It is the first file that is loaded when the app is started.
+//  It is used to define the layout of the app.
+// All of the components that are used in the app are imported here.
+// all of the component are wrapped in the RootLayout component.
