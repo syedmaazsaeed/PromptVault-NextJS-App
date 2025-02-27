0ca918efdaf204d1e4e398c4e569df3b3696b998
syedmaazsaeed <syedmaazsaeed@gmail.com>
2025-02-28 00:28:02 +0500

    Updated the Navbar
    Adding Sign in Sign up features

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,18 @@ import { signIn, signOut, useSession, getProviders  } from 'next-auth/react';
 
 const Navbar = () => {
 
-    const isUserLoggedIn = true;
+    const isUserLoggedIn = false;
+
+    const [providers, setProviders] = useState(null);
+
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders();
+            setProviders(response);
+        }
+    }, [])
+
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
         <Link href="/" className='flex gap-2 flex-center'>
@@ -19,7 +30,7 @@ const Navbar = () => {
         />
         <p className='logo_text'>PromptVault</p>
         </Link>
-        {/* Smart Devices/Mobile Phone devices Navigation */}
+        {/* Desktop Navigation */}
         <div className='sm:flex hidden'></div>
         {isUserLoggedIn ? (
             <div className='flex gap-3 md:gap-5'>
@@ -27,10 +38,27 @@ const Navbar = () => {
                 className='black_btn'>
                 Create Post 
                 </Link>
+
+                <button type="button" onClick={signOut} className='outline_btn'>
+                    Sign Out
+
+                </button>
+
+                <Link href="/profile">
+                <Image
+                src="assets/images/logo.svg"
+                width={38}
+                height={37}
+                className='rounded-full'
+                alt='Profile-Avatar'
+                />
+                
+                </Link>
             </div>
 
         ): (
             <>
+
             </>
         )}
     </nav>
